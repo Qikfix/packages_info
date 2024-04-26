0f2593105d2dc2a065a6cfecf5710ef2a79d5b33
Waldirio M Pinheiro <waldirio@gmail.com>
2024-04-25 19:39:55 -0700

    tabs removed

--- packages_info.sh
+++ packages_info.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 if [ "$1" == "--full" ]; then
-	echo "Collecting all the packages information"
+  echo "Collecting all the packages information"
 elif [ "$1" == "--package" ] && [ "$2" != "" ]; then
   echo "Searching by package $2"
 else
   echo "Please, call $0 with the parameter"
   echo " --full # To generate a complete list of all the packages in the content view"
   echo " --package <package name> # To generate a list of packages in the content view, based on the name"
-	echo
+  echo
   echo "exiting ..."
-	exit
+  exit
 fi
 
 
@@ -39,56 +39,56 @@ do
   #echo "- $cv_id,\"$cv_name\",$cv_label,$cv_last_publish"
 
   # Generating a list of CVs by version
-	hammer --csv --no-headers content-view version list | while read cvv
-	do
-		cvv_id=$(echo $cvv | cut -d, -f1)
-		cvv_name_report=$(echo $cvv | cut -d, -f2)
-		cvv_name=$(echo $cvv | cut -d, -f2 | sed 's/ [0-9].[0-9]//g')
-		cvv_version=$(echo $cvv | cut -d, -f3)
-		cvv_desc=$(echo $cvv | cut -d, -f4)
-		cvv_lfc=$(echo $cvv | cut -d, -f5)
+  hammer --csv --no-headers content-view version list | while read cvv
+  do
+    cvv_id=$(echo $cvv | cut -d, -f1)
+    cvv_name_report=$(echo $cvv | cut -d, -f2)
+    cvv_name=$(echo $cvv | cut -d, -f2 | sed 's/ [0-9].[0-9]//g')
+    cvv_version=$(echo $cvv | cut -d, -f3)
+    cvv_desc=$(echo $cvv | cut -d, -f4)
+    cvv_lfc=$(echo $cvv | cut -d, -f5)
 
-		#echo "-- $cvv_id,$cvv_name,$cvv_version,$cvv_desc,$cvv_lfc"
+    #echo "-- $cvv_id,$cvv_name,$cvv_version,$cvv_desc,$cvv_lfc"
 
-		# Matching the CV with the version
-		if [ "$cv_name" == "$cvv_name" ]; then
-			#echo "-- $cvv_id,$cvv_name,$cvv_version,$cvv_desc,$cvv_lfc"
-			#hammer --csv --no-headers package list --content-view-id 2 --content-view-version-id 5 --search 'filename ~ gofer' --fields filename
-			if [ "$2" == "" ]; then
- 				command="hammer --csv --no-headers package list --content-view-id $cv_id --content-view-version-id $cvv_id --fields filename"
-			else
- 				command="hammer --csv --no-headers package list --content-view-id $cv_id --content-view-version-id $cvv_id --search filename~${2} --fields filename"
-			fi
-			# Based on the parameters, this will pass through all the packages or only for the selected ones
-			$(echo $command) | while read pkg_name
-			do
-				if [ "$pkg_name" != "" ]; then
-        	#echo "AUDIT: cv: $cv_name, cvv: $cvv_name_report, package: '$pkg_name'"
-					count=$(grep "| $pkg_name" $ERRATA_FILE | wc -l)
-        	#echo "AUDIT: count: $count"
-					if [ "$count" -eq 0 ]; then
-						:
-					elif [ "$count" -eq 1 ]; then
-						#echo "- $pkg_name"
-						errata_list=$(grep "| $pkg_name" $ERRATA_FILE | awk '{print $3}')
-						errata_type=$(grep "| $pkg_name" $ERRATA_FILE | awk '{print $5}')
-						errata_sev=$(grep "| $pkg_name" $ERRATA_FILE | awk '{print $7}')
-						# Generating the content of the CSV
-						echo "$pkg_name,$cv_id,$cv_name,$cv_label,$cv_last_publish,$cvv_id,$cvv_name_report,$cvv_version,$cvv_description,$cvv_lfc,$errata_list,$errata_type,$errata_sev" >>$OUTPUT_FILE
-					else
-					  grep "| $pkg_name" $ERRATA_FILE | while read each_errata
-					  do
-							errata_list=$(echo $each_errata | awk '{print $3}')
-							errata_type=$(echo $each_errata | awk '{print $5}')
-							errata_sev=$(echo $each_errata | awk '{print $7}')
-							# Generating the content of the CSV
-							echo "$pkg_name,$cv_id,$cv_name,$cv_label,$cv_last_publish,$cvv_id,$cvv_name_report,$cvv_version,$cvv_description,$cvv_lfc,$errata_list,$errata_type,$errata_sev" >>$OUTPUT_FILE
-					  done
-					fi
-				fi
-			done
-		fi
-	done
+    # Matching the CV with the version
+    if [ "$cv_name" == "$cvv_name" ]; then
+      #echo "-- $cvv_id,$cvv_name,$cvv_version,$cvv_desc,$cvv_lfc"
+      #hammer --csv --no-headers package list --content-view-id 2 --content-view-version-id 5 --search 'filename ~ gofer' --fields filename
+      if [ "$2" == "" ]; then
+        command="hammer --csv --no-headers package list --content-view-id $cv_id --content-view-version-id $cvv_id --fields filename"
+      else
+        command="hammer --csv --no-headers package list --content-view-id $cv_id --content-view-version-id $cvv_id --search filename~${2} --fields filename"
+      fi
+      # Based on the parameters, this will pass through all the packages or only for the selected ones
+      $(echo $command) | while read pkg_name
+      do
+        if [ "$pkg_name" != "" ]; then
+          #echo "AUDIT: cv: $cv_name, cvv: $cvv_name_report, package: '$pkg_name'"
+          count=$(grep "| $pkg_name" $ERRATA_FILE | wc -l)
+          #echo "AUDIT: count: $count"
+          if [ "$count" -eq 0 ]; then
+            :
+          elif [ "$count" -eq 1 ]; then
+            #echo "- $pkg_name"
+            errata_list=$(grep "| $pkg_name" $ERRATA_FILE | awk '{print $3}')
+            errata_type=$(grep "| $pkg_name" $ERRATA_FILE | awk '{print $5}')
+            errata_sev=$(grep "| $pkg_name" $ERRATA_FILE | awk '{print $7}')
+            # Generating the content of the CSV
+            echo "$pkg_name,$cv_id,$cv_name,$cv_label,$cv_last_publish,$cvv_id,$cvv_name_report,$cvv_version,$cvv_description,$cvv_lfc,$errata_list,$errata_type,$errata_sev" >>$OUTPUT_FILE
+          else
+            grep "| $pkg_name" $ERRATA_FILE | while read each_errata
+            do
+              errata_list=$(echo $each_errata | awk '{print $3}')
+              errata_type=$(echo $each_errata | awk '{print $5}')
+              errata_sev=$(echo $each_errata | awk '{print $7}')
+              # Generating the content of the CSV
+              echo "$pkg_name,$cv_id,$cv_name,$cv_label,$cv_last_publish,$cvv_id,$cvv_name_report,$cvv_version,$cvv_description,$cvv_lfc,$errata_list,$errata_type,$errata_sev" >>$OUTPUT_FILE
+            done
+          fi
+        fi
+      done
+    fi
+  done
 done
 
 echo "Please, check $OUTPUT_FILE"
